4ce6ab977492457e7c1627cd7587620480ac2e34
RaxLaurent <fadjarw28@gmail.com>
2025-10-28 02:53:52 +0700

    Update flower animations and text content for improved visuals and user interaction

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Click open dibawah ini yaa'.split('')
+const text = 'Pencet Open'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('SEMANGAT JOOO').split('')
+    const titles = ('For my beloved,Nima').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
@@ -11,7 +11,7 @@ onload = () => {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        setTimeout(appendTitle, 200); // 1000ms delay
       }
     }
 
